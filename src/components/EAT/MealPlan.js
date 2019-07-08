6478861e7b118a3334934cea850f6574f2e4fc15
Levi Winans <levieiko@gmail.com>
2019-07-08 14:42:07 -0700

    horizontal scroll bar created for mealplan

--- src/components/EAT/MealPlan.js
+++ src/components/EAT/MealPlan.js
@@ -8,37 +8,69 @@ export default class MealPlan extends Component {
         return(
             <>
             <h1>My Weekly Nutrition Program</h1>
-            <div className="flex-container">
-                <section>
+            <div className="outer-scroll">
+                <div className="inner-scroll">
+                    <section>
+                            <Link to={routes.PN1}>
+                        <div className="image">
+                            <img src="../smoothie.png" alt="nutrition1" />
+                        </div>
+                            </Link>
+                        <div className="title">
+                            <span>Nutrition Point 1</span>
+                        </div>
+                    </section>
+                    <section>
                         <Link to={routes.PN1}>
-                    <div className="image">
-                        <img src="../smoothie.png" alt="nutrition1" />
-                    </div>
+                        <div className="image">
+                        <img src="../fruit.png" alt="nutrition2" />
+                        </div>
                         </Link>
-                    <div className="title">
-                        <span>Nutrition Point 1</span>
-                    </div>
-                </section>
-                <section>
-                    <Link to={routes.PN1}>
-                    <div className="image">
-                    <img src="../fruit.png" alt="nutrition2" />
-                    </div>
-                    </Link>
-                    <div className="title">
-                        <span>Nutrition Point 2</span>
-                    </div>
-                </section>
-                <section>
-                    <Link to={routes.PN1}>
-                    <div className="image">
-                    <img src="../pizza.png" alt="nutrition3" />
-                    </div>
-                    </Link>
-                    <div className="title">
-                        <span>Nutrition Point 3</span>
-                    </div>
-                </section>
+                        <div className="title">
+                            <span>Nutrition Point 2</span>
+                        </div>
+                    </section>
+                    <section>
+                        <Link to={routes.PN1}>
+                        <div className="image">
+                        <img src="../pizza.png" alt="nutrition3" />
+                        </div>
+                        </Link>
+                        <div className="title">
+                            <span>Nutrition Point 3</span>
+                        </div>
+                    </section>
+                    <section>
+                        <Link to={routes.PN1}>
+                        <div className="image">
+                        <img src="../pizza.png" alt="nutrition3" />
+                        </div>
+                        </Link>
+                        <div className="title">
+                            <span>Nutrition Point 3</span>
+                        </div>
+                    </section>
+                    <section>
+                        <Link to={routes.PN1}>
+                        <div className="image">
+                        <img src="../pizza.png" alt="nutrition3" />
+                        </div>
+                        </Link>
+                        <div className="title">
+                            <span>Nutrition Point 3</span>
+                        </div>
+                    </section>
+                    <section>
+                        <Link to={routes.PN1}>
+                        <div className="image">
+                        <img src="../pizza.png" alt="nutrition3" />
+                        </div>
+                        </Link>
+                        <div className="title">
+                            <span>Nutrition Point 3</span>
+                        </div>
+                    </section>
+                </div>
             </div>
             </>
         )
