768ac1c4732db4f1557c78268f3478819cb3b078
Levi Winans <levieiko@gmail.com>
2019-07-08 16:36:53 -0700

    add images

--- src/components/EAT/EatBlog.js
+++ src/components/EAT/EatBlog.js
@@ -13,10 +13,10 @@ export default class EatBlog extends Component {
                     <div className="babeSci">
                         <div className="content">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </p>
                         </div>
-                        <div className="photo"><img src="../sci.png" alt="eat blog" /></div>
+                        <div className="photo"><img src="../eat.png" alt="eat blog" /></div>
                     </div>
                 </section>
             </div>
--- src/components/LIFT/LiftBlog.js
+++ src/components/LIFT/LiftBlog.js
@@ -16,7 +16,7 @@ export default class LiftBlog extends Component {
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             </p>
                         </div>
-                        <div className="photo"><img src="../sci.png" alt="LifBlog"/></div>
+                        <div className="photo"><img src="../friend.png" alt="LifBlog"/></div>
                     </div>
                 </section>
             </div>
