428fce5891e0ec72ca6640e698ad40c8c2f1f42b
Levi Winans <levieiko@gmail.com>
2019-07-08 17:44:10 -0700

    added links to bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,6 +6,9 @@ export default class Bio extends Component {
             <>
                 <h1>Say Hello to Coach Momo</h1>
                 <img className="imgBio" src="../headshot.png" />
+                <br />
+                <a href="https://www.facebook.com/BabeGoLift/"><img className="iconBio" src="../facebook-icon.png" /></a>
+                <a href="https://www.instagram.com/momomuscle/"><img className="iconBio" src="../instagram-icon.png" /></a>
             </>
         )
     }
