fcf32c3d07d92ca1da0b1680a4ff0b4b8713bacd
Levi Winans <levieiko@gmail.com>
2019-07-08 18:15:53 -0700

    clean up code on imgs

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ export default class Bio extends Component {
         return(
             <>
                 <h1>Say Hello to Coach Momo</h1>
-                <img src="../headshot.png" alt="bioPic"/>
+                <img src="../headshot.png" alt="bio-pic"/>
                 <section>
                     <a href="https://www.facebook.com/BabeGoLift/"><img src="../facebook-icon.png" alt="fb-icon"/></a>
                     <a href="https://www.instagram.com/momomuscle/"><img src="../instagram-icon.png" alt="insta-icon"/></a>
