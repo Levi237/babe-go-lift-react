c484a7530ad3a8ce2cc0421e078510e4304fa771
Levi Winans <levieiko@gmail.com>
2019-07-14 20:52:27 -0700

    swap colors on blog component

--- src/components/LIFT/LiftBlog.js
+++ src/components/LIFT/LiftBlog.js
@@ -4,14 +4,12 @@ export default class LiftBlog extends Component {
     render(){
         return(
             <>
-            <h1>Babe, Go Lift - Blogs</h1>
             <div className="header-container">
                 <section>
                     <div className="babeSci">
                         <div className="content">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            </p>
+                        <h1>Babe, Go Lift - Blogs</h1>
+                            <span>This is an attempt per our convo Sunday</span>
                         </div>
                         <div className="photo"><img src="../friend.png" alt="LifBlog"/></div>
                     </div>
